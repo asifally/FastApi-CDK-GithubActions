8cc1a48d7c686d53fc1e8246d191b9bf73e4e2b0
Asif Ally <asifally004@gmail.com>
2023-01-21 23:05:06 -0600

    Black formatting

--- cdk/cdk_stack.py
+++ cdk/cdk_stack.py
@@ -11,9 +11,7 @@ class AppStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        service.AppService(
-            self, "AnimeQuoteGenerator"
-        )
+        service.AppService(self, "AnimeQuoteGenerator")
         # The code that defines your stack goes here
 
         # example resource
